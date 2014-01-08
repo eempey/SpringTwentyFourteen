bbee44ef584680dd40bf04e39e72cadd289421b3
eempey <eempey@server.fake>
2014-01-07 19:01:24 -0800

    Menu plugins & jscript enqueue

--- footer.php
+++ footer.php
@@ -2,26 +2,16 @@
 </div>
 <footer>
   <div class="container">
-    <form class="form-horizontal">
-      <legend>Sign up for our mailing list</legend>
-      <div class="form-group">
-        <label class="sr-only" for="mailingListEmail">Email</label>
-        <input name="Email" type="text" class="form-control" id="mailingListEmail" placeholder="Email" size="20">
-      </div>
-      <div class="form-group">
-        <label class="sr-only" for="mailingListCities">Cities</label>
-        <input name="Cities" type="text" class="form-control" id="mailingListCities" placeholder="Cities" size="20">
-      </div>
-      <button type="submit" class="btn btn-default">Submit</button>
-    </form>
-    <div class="pull-right"><small class="copyright">Spr&iuml;ng 2013-2014</small>
+
+    <div class="pull-right"><small class="copyright"><?php bloginfo('name'); ?> 2013-<?php echo date('Y'); ?></small>
       <ul class="social-media-icons">
-        <li><a href="#" target="_blank" class="facebook"></a></li>
-        <li><a href="#" target="_blank" class="twitter"></a></li>
-        <li><a href="#" target="_blank" class="bandcamp"></a></li>
+        <li><a href="#" title="facebook" target="_blank" class="facebook"></a></li>
+        <li><a href="#" title="twitter" target="_blank" class="twitter"></a></li>
+        <li><a href="#" title="bandcamp" target="_blank" class="bandcamp"></a></li>
         </ul>
     </div>
   </div>
+  <?php wp_footer(); ?>
 </footer>
 </body>
 </html>
--- functions.php
+++ functions.php
@@ -1,4 +1,27 @@
-<?php function register_my_menu() {
+<?php 
+
+//Register the Main menu
+function register_my_menu() {
   register_nav_menu('header-menu',__( 'Header Menu' ));
 }
-add_action( 'init', 'register_my_menu' ); ?>
+add_action( 'init', 'register_my_menu' ); 
+
+
+// Register Custom Navigation Walker
+require_once('wp_bootstrap_navwalker.php');
+
+//register javascripts
+function spring_scripts_with_jquery()  
+	{  
+		// Register the script like this for a theme:  
+		wp_register_script( 'bootstrap', '//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js', array( 'jquery' ) );  
+  		wp_register_script( 'bootstrap-dropdown-hover', get_template_directory_uri().'/js/bootstrap-hover-dropdown.js', array( 'jquery', 'bootstrap') );
+		// For either a plugin or a theme, you can then enqueue the script:  
+		wp_enqueue_script( 'bootstrap' );  
+		wp_enqueue_script( 'bootstrap-dropdown-hover' );
+}  
+add_action( 'wp_enqueue_scripts', 'spring_scripts_with_jquery' );  
+
+
+
+?>
