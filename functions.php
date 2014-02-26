68cce175e054697d2bcbd509455bccec3919e336
eempey <eempey@server.fake>
2014-02-25 18:01:36 -0800

    Styling updates

--- functions.php
+++ functions.php
@@ -16,27 +16,33 @@ require_once('wp_bootstrap_navwalker.php');
 //Kill Woocommerce styles
 define('WOOCOMMERCE_USE_CSS', false);
 
+
 //then add my own
 function theme_enqueue_styles() {
     wp_enqueue_style('woocommerce-layout', get_stylesheet_directory_uri().'/woocommerce/css/woocommerce-layout.css');
     wp_enqueue_style('woocommerce-smallscreen', get_stylesheet_directory_uri().'/woocommerce/css/woocommerce-smallscreen.css','','','only screen and (max-width: 768px)');
     wp_enqueue_style('woocommerce-custom', get_stylesheet_directory_uri().'/woocommerce/css/woocommerce.css');
+    //I'll handle the styling for Jetpack's Grunion Contact Form:
+    wp_deregister_style('grunion.css');
 }
 
 add_action('init', 'theme_enqueue_styles');
 
 //register javascripts please
-function spring_scripts_with_jquery()  
-	{  
-		// Register the script like this for a theme: 
-        //wp_register_script( 'less', '//cdnjs.cloudflare.com/ajax/libs/less.js/1.6.3/less.min.js'); 
+function spring_scripts_with_jquery()  {  
+	if ( !is_admin() ) {
+    	// Register the script like this for a theme:  
 		wp_register_script( 'bootstrap', '//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js', array( 'jquery' ) );  
   		wp_register_script( 'bootstrap-dropdown-hover', get_template_directory_uri().'/js/bootstrap-hover-dropdown.js', array( 'jquery', 'bootstrap') );
-		// For either a plugin or a theme, you can then enqueue the script:  
-		//wp_enqueue_script( 'less' );  
+        //wp_register_script( 'erin', get_template_directory_uri().'/js/erin.js', array( 'jquery'),'',true );
+		
+        // For either a plugin or a theme, you can then enqueue the script:   
         wp_enqueue_script( 'bootstrap' );  
 		wp_enqueue_script( 'bootstrap-dropdown-hover' );
-
+        /*if ( is_page('Contact') ){
+            wp_enqueue_script( 'erin' ); 
+        }*/
+    } 
 }  
 add_action( 'wp_enqueue_scripts', 'spring_scripts_with_jquery' );  
 
@@ -75,8 +81,8 @@ function responsive_custom_background_cb() {
         wp_reset_postdata();//not resetting this caused the image to show up on woocommerce page description
 
         $backgroundFull = wp_get_attachment_image_src($backgroundId, "full")[0];
-        $backgroundLarge = wp_get_attachment_image_src($backgroundId, [1024,768])[0]; 
-        $backgroundMobile = wp_get_attachment_image_src($backgroundId, [768,767])[0];   
+        $backgroundLarge = wp_get_attachment_image_src($backgroundId, [1170,780])[0]; 
+        $backgroundMobile = wp_get_attachment_image_src($backgroundId, [768,512])[0];   
   
         $repeat = get_theme_mod( 'background_repeat', 'repeat' );  
   
@@ -115,13 +121,17 @@ function responsive_custom_background_cb() {
 // ?>
 
  jQuery(document).ready(function($) {
-        if(window.screen.width > 1024){
-            $('body.custom-background').css('background-image', <?php echo "'url(".$backgroundFull.")'" ?> );
-        }else if(window.screen.width > 500 && window.screen.width <= 1024){
-            $('body.custom-background').css('background-image', <?php echo "'url(".$backgroundLarge.")'" ?> );
-        }else{
-            $('body.custom-background').css('background-image', <?php echo "'url(".$backgroundMobile.")'" ?> );
-        }    
+    var windowWidth = $(window).width();
+    var windowHeight = $(window).height();
+
+    if($(window).width() > 1170){
+        $('body.custom-background').css('background-image', <?php echo "'url(".$backgroundFull.")'" ?> );
+    }else if($(window).width() > 768 && $(window).width() <= 1170){
+        $('body.custom-background').css('background-image', <?php echo "'url(".$backgroundLarge.")'" ?> );
+    }else{
+        $('body.custom-background').css('background-image', <?php echo "'url(".$backgroundMobile.")'" ?> );
+        
+    }    
     });
 
 
